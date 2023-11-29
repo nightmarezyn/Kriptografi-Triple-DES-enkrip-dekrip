8295b8828b6a7efec55eec957a3147de4928d2c8
nightmarezyn <111229380+nightmarezyn@users.noreply.github.com>
2023-11-29 22:07:18 +0700

    Update Home.py

--- Home/Home.py
+++ Home/Home.py
@@ -3,10 +3,10 @@ import streamlit as st
 st.title("Triple DES ALGORITHM")
 
 st.subheader('Proyek Akhir Mata Kuliah Kriptografi - Kelompok 2', divider='blue')
-st.markdown("Rahman Ilyas Al Kahfi        (2210511044)")
-st.markdown("Salwa Nafisa                 (2210511051)") 
-st.markdown("Intan Febyola Putri Dwina S. (2210511065)")
-st.markdown("Muhammad Nur Alam            (2210511068)") 
-st.markdown("Farel Bayhaqi                (2210511073)")
+st.markdown(- "Rahman Ilyas Al Kahfi        (2210511044)")
+st.markdown(- "Salwa Nafisa                 (2210511051)") 
+st.markdown(- "Intan Febyola Putri Dwina S. (2210511065)")
+st.markdown(- "Muhammad Nur Alam            (2210511068)") 
+st.markdown(- "Farel Bayhaqi                (2210511073)")
 
 st.subheader('Triple DES', divider='blue', anchor="/google")
