1fa783d6283c2acdbf313b503c6a0333356119f2
nightmarezyn <111229380+nightmarezyn@users.noreply.github.com>
2023-12-01 14:08:24 +0700

    Update REAL3DES.py

--- TripleDES/REAL3DES.py
+++ TripleDES/REAL3DES.py
@@ -81,27 +81,27 @@ def home_page():
 
 # EEE Explanation
     with st.expander("EEE (Encrypt, Encrypt, Encrypt)"):
-    st.markdown("""
-    Pada mode ini, teks terbuka dienkripsi tiga kali secara berurutan menggunakan kunci yang berbeda pada setiap langkah.
-    Misalnya, jika kita menyebut kunci pertama sebagai K1, kunci kedua sebagai K2, dan kunci ketiga sebagai K3, 
-    maka proses enkripsi akan seperti berikut:
-
-    ```
-    CipherText = Encrypt(K1, Encrypt(K2, Encrypt(K3, PlainText)))
-    ```
-    """)
+        st.markdown("""
+        Pada mode ini, teks terbuka dienkripsi tiga kali secara berurutan menggunakan kunci yang berbeda pada setiap langkah.
+        Misalnya, jika kita menyebut kunci pertama sebagai K1, kunci kedua sebagai K2, dan kunci ketiga sebagai K3, 
+        maka proses enkripsi akan seperti berikut:
+        
+        ```
+        CipherText = Encrypt(K1, Encrypt(K2, Encrypt(K3, PlainText)))
+        ```
+        """)
 
 # EDE Explanation
     with st.expander("EDE (Encrypt, Decrypt, Encrypt)"):
-    st.markdown("""
-    Pada mode ini, teks terbuka dienkripsi, kemudian didekripsi, dan akhirnya dienkripsi lagi.
-    Misalnya, jika kita menyebut kunci pertama sebagai K1, kunci kedua sebagai K2, dan kunci ketiga sebagai K3, 
-    maka proses enkripsi-dekripsi-enkripsi akan seperti berikut:
-
-    ```
-    CipherText = Encrypt(K1, Decrypt(K2, Encrypt(K3, PlainText)))
-    ```
-    """)
+        st.markdown("""
+        Pada mode ini, teks terbuka dienkripsi, kemudian didekripsi, dan akhirnya dienkripsi lagi.
+        Misalnya, jika kita menyebut kunci pertama sebagai K1, kunci kedua sebagai K2, dan kunci ketiga sebagai K3, 
+        maka proses enkripsi-dekripsi-enkripsi akan seperti berikut:
+        
+        ```
+        CipherText = Encrypt(K1, Decrypt(K2, Encrypt(K3, PlainText)))
+        ```
+        """)
 
 # Encryption page
 def encrypt_page():
